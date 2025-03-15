d67f80a3cfd84f233c1cebf26f9a96acc227af2f
rice <rice9547@gmail.com>
2025-03-16 01:52:34 +0800

    Refactor goal handling to utilize UserID instead of email, updating related tests and context management for improved user identification.

--- internal/interfaces/http/handler_test.go
+++ internal/interfaces/http/handler_test.go
@@ -1,8 +1,10 @@
 package handler_test
 
 import (
+	"context"
 	"testing"
 
+	"github.com/Financial-Partner/server/internal/contextutil"
 	"github.com/Financial-Partner/server/internal/infrastructure/logger"
 	handler "github.com/Financial-Partner/server/internal/interfaces/http"
 	"go.uber.org/mock/gomock"
@@ -26,4 +28,12 @@ func newTestHandler(t *testing.T) (*handler.Handler, *MockServices) {
 	h := handler.NewHandler(ms.UserService, ms.AuthService, ms.GoalService, logger.NewNopLogger())
 
 	return h, ms
+}
+
+func newContext(userID, userEmail string) context.Context {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, contextutil.UserIDKey, userID)
+	ctx = context.WithValue(ctx, contextutil.UserEmailKey, userEmail)
+
+	return ctx
 }
